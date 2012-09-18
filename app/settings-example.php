1b53b6d8bf6ad4556b9d545624e34d8991b7fe44
stamit <stamit@stamit.gr>
2012-09-18 10:34:54 +0300

    better settings example

--- app/settings-example.php
+++ app/settings-example.php
@@ -1,7 +1,7 @@
 <?
 $URL = '/food';  # delete this line if documents are at ServerRoot
-$DB = 'mysql://username:password@serveraddress/food?persist';
-$NDB = 'mysql://username:password@serveraddress/food?persist';
+$DB = 'mysql://username:password@localhost/food?persist';
+$NDB = 'mysql://username:password@localhost/food?persist';
 $AUTH = 'user';
 $COUNTRY_CODE = '+30';
 #$PRODUCTION_SERVER = true;  # shy mode
