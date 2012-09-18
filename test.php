23354a3b575d9732871a27bb4fefd0f72fa0e133
stamit <stamit@stamit.gr>
2012-09-18 09:43:33 +0300

    test page

--- test.php
+++ test.php
@@ -1,16 +1,7 @@
-<? $AUTH='admin';
+<?
 	require_once 'app/init.php';
-
-	if (posting()) try {
-		error_log('TEST '.repr($_POST));
-		if (success()) return true;
-	} catch (Exception $x) {
-		if (failure($x)) return false;
-	}
 ?>
 <? include 'app/begin.php' ?>
-<form method="post" action="#">
-<input type="text" name="foo" value="bar" />
-<button type="submit">Submit</button>
-</form>
+<h1>This is a test</h1>
+<p>Welcome to the test page.</p>
 <? include 'app/end.php' ?>
