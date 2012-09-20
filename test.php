e55c4912a2686a3559522722a562cd3a069e9219
stamit <stamit@stamit.gr>
2012-09-20 14:23:14 +0300

    compatible test

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
-<?
+<?php
 	require_once 'app/init.php';
 ?>
-<? include 'app/begin.php' ?>
+<?php include 'app/begin.php' ?>
 <h1>This is a test</h1>
 <p>Welcome to the test page.</p>
-<? include 'app/end.php' ?>
+<?php include 'app/end.php' ?>
