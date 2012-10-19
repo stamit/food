5b64eff3149955391b16b140587e525468344e2e
stamit <stamit@stamit.gr>
2012-10-19 03:11:21 +0300

    better USDA-format import/exprt scripts

---
+++ doc/usda-export-mysql.py
@@ -0,0 +1,124 @@
+import sys
+import MySQLdb
+import MySQLdb.connections
+import MySQLdb.cursors
+
+host = 'localhost'
+dbname = 'food'
+charset = 'utf8'
+tableprefix = 'usda_'
+#tableprefix = ''
+
+def sqlid(s):
+	return '`'+s.replace('`','``')+'`'
+
+def get_tp_size(col):
+	tp = str(col[1])
+	p1 = tp.find('(')
+	p2 = tp.find(')')
+	if p1 and p2 and p1<p2:
+		typename = tp[:p1].upper()
+		size = tp[p1+1:p2].replace(',','.')
+	else:
+		typename = tp.upper()
+		size = '-'
+	size = ''.join(size.split()).split('.')
+	return typename,size
+
+def sql(x,db):
+	if x==None:
+		return 'NULL'
+	elif type(x)==int:
+		return str(x)
+	elif type(x)==str:
+		return "'"+db.escape_string(x)+"'"
+	else:
+		raise 'unimplemented'
+
+def eatprefix(x,y):
+	if not x.startswith(y):
+		return None
+	return x[len(y):]
+
+if __name__=='__main__':
+	db = MySQLdb.connections.Connection(host=host, db=dbname, charset=charset)
+
+	c = db.cursor()
+	c.execute('SHOW TABLES')
+
+	tables = []
+	for row in c.fetchall():
+		tables.append(str(row[0]))
+	
+	tables.sort()
+
+	c.execute("SELECT table_name, column_name, referenced_table_name, referenced_column_name FROM information_schema.key_column_usage WHERE referenced_table_name IS NOT NULL AND table_schema="+sql(dbname,db))
+	foreign_keys = c.fetchall()
+
+	for table in tables:
+		table = eatprefix(table,tableprefix)
+		if not table:
+			continue
+
+		c.execute('SHOW FULL COLUMNS FROM '+sqlid(tableprefix+table))
+
+		sys.stdout.write('\n'+table+'\n')
+
+		cols = c.fetchall()
+
+		maxlen = reduce(lambda a,b: max(a,b), map(lambda a:len(a[0]), cols))
+
+		for col in cols:
+			name = col[0].encode('utf8')
+
+			typename,size = get_tp_size(col)
+			if typename=='INT':
+				size = '-'
+			if typename=='TINYINT':
+				typename = 'INT';
+				size = '-';
+			if typename=='DOUBLE':
+				typename = 'FLOAT8'
+				size = '-'
+
+			if typename=='VARCHAR':
+				typename = 'A'
+			if typename=='DECIMAL':
+				typename = 'N'
+				if size[1]=='0':
+					size = size[0]
+
+			'_'.join(typename.split())
+
+			if col[3]=='NO':
+				nullable = 'N'
+			else:
+				nullable = 'Y'
+
+			if col[4]=='PRI':
+				pri = '*'
+			else:
+				pri = ''
+
+			comment = col[8].encode('utf8')
+			comment.replace('\n',' ')
+			if comment:
+				comment = '\t--'+comment
+
+			refs = filter(lambda a: (a[0]==tableprefix+table and a[1]==col[0]), foreign_keys)
+			if len(refs)>1 and not reduce(lambda a,b: (a==b) and a, refs):
+				raise Exception('strange field references more than one foreign key: %s.%s: %s'%(table,name,repr(refs)))
+			if refs:
+				foreign_table = eatprefix(refs[0][2].encode('utf8'),tableprefix)
+				if foreign_table<>None:
+					ref = ' '+foreign_table+'.'+refs[0][3].encode('utf8')
+				else:
+					ref = ''
+			else:
+				ref = ''
+
+			sys.stdout.write('%s%s %s %s%s %s%s%s\n'%(name, ' '*(maxlen-len(col[0])), typename, '.'.join(size),pri, nullable, ref, comment))
+
+	c.close()
+
+	db.close()
--- doc/usda-import.py
+++ doc/usda-import.py
@@ -2,31 +2,73 @@
 # * reads schema.txt as standard input and UPPERCASE.txt files in working directory
 # * produces SQL statements for importing database into MySQL or PostgreSQL
 #
+# with one argument it outputs only CREATE TABLE statements
+#
+# with two arguments it outputs ALTER TABLE statements for modifying the first
+# schema to match the second
+#
 # any table reference cycles might not be handled properly
+#
+# cannot drop foreign keys
+#
+# XXX renaming a column will cause it to be dropped; KEEP NAMES UNCHANGED
+#
+# XXX changing primary keys is not implemented
 #
 import sys
 import re
 
-# for MySQL
 droptables = 1
 writeschema = 1
+writedata = 1
+tableprefix = 'usda_'
+
+# for MySQL
 fieldquote = '`'
 commentsyntax = 0
-writedata = 1
 recordgroup = 16  # to avoid MySQL "packet too large" errors
-tableprefix = ''
+mysql = 1
+typemap = {
+	'A':'VARCHAR',
+	'VARCHAR':'VARCHAR',
+	'TEXT':'TEXT',
+
+	'BOOL':'TINYINT',
+	'BOOLEAN':'TINYINT',
+	'INT':'INT',
+	'N':'DECIMAL',
+
+	'FLOAT':'FLOAT',
+	'FLOAT8':'DOUBLE',
+	'DOUBLE':'DOUBLE',
+
+	'DATE':'DATE',
+	'TIME':'TIME',
+	'TIMESTAMP':'TIMESTAMP',
+}
 
 # for PostreSQL
-#droptables = 1
-#writeschema = 1
 #fieldquote = '"'
 #commentsyntax = 1
-#writedata = 1
 #recordgroup = 16
-#tableprefix = ''
-
-if len(sys.argv)>=2:
-	tableprefix = sys.argv[1]
+#typemap = {
+#	'A':'VARCHAR',
+#	'VARCHAR':'VARCHAR',
+#	'TEXT':'TEXT',
+#
+#	'BOOL:'BOOLEAN',
+#	'BOOLEAN':'BOOLEAN',
+#	'INT':'INT',
+#	'N':'DECIMAL',
+#
+#	'FLOAT':'FLOAT',
+#	'FLOAT8':'FLOAT8',
+#	'DOUBLE':'FLOAT8',
+#
+#	'DATE':'DATE',
+#	'TIME':'TIME',
+#	'TIMESTAMP':'TIMESTAMP WITH TIME ZONE',
+#}
 
 def readschema(f):
 	lineno = 1
@@ -71,13 +113,18 @@ def readschema(f):
 				else:
 					keyfield = False
 
-				if words[1]<>'A' and words[1]<>'N':
-					raise Exception("expected 'A' or 'N' as second word: %s"%line)
+				if typemap.get(words[1])==None:
+					raise Exception("unrecognized type (second word): %s"%line)
+				words[1] = typemap[words[1]]
 
-				if words[2].find('.')==-1:
-					words[2] = int(words[2])
+				if words[2] == '-':
+					words[2] = None
 				else:
 					words[2] = tuple(map(int,words[2].split('.')))
+					while len(words[2]) and words[2][-1]==0:
+						words[2] = words[2][:-1]
+					if len(words[2])==1:
+						words[2] = words[2][0]
 
 				if words[3]=='Y':
 					words[3] = True
@@ -123,16 +170,22 @@ def iskey(words):
 def field_def(words,singlekeyfield=0):
 	t = [sqlfield(words[0])]
 
-	if words[1]=='A':
+	if words[1]=='VARCHAR':
 		t.append('VARCHAR(%d)'%words[2])
 	else:
 		if type(words[2])==tuple:
-			s = '%d,%d'%(words[2][0],words[2][1])
+			s = ','.join(map(str,words[2]))
+		elif words[2]<>None:
+			s = str(words[2])
 		else:
-			s = '%d'%words[2]
-		t.append('DECIMAL(%s)'%s)
+			s = None
 
-	if words[3]=='N':
+		if s<>None:
+			t.append('%s(%s)'%(words[1],s))
+		else:
+			t.append(words[1])
+
+	if not words[3]:
 		t.append('NOT NULL')
 
 	if singlekeyfield and words[5]:
@@ -140,9 +193,6 @@ def field_def(words,singlekeyfield=0):
 
 	if commentsyntax==0 and words[6]<>None:
 		t.append('COMMENT '+sqlstring(words[6]))
-
-	if words[4]:
-		t.append('REFERENCES %s.%s'%(sqlfield(tableprefix+words[4][0]),sqlfield(words[4][1])))
 
 	return ' '.join(t)
 
@@ -152,9 +202,12 @@ def field_def_pkey(s):
 def typed_value(value,field):
 	return sqlstring(value)
 
-def print_inserts(tablename,fields):
+def print_table(tablename,fields):
 	filename = tablename.upper()+'.txt'
-	f = open(filename,'r')
+	try:
+		f = open(filename,'r')
+	except IOError, x:
+		return
 
 	lineno = 0
 	outlineno = 0
@@ -210,70 +263,165 @@ def print_inserts(tablename,fields):
 		sys.stdout.write(';\n')
 
 	f.close()
-
-def print_creates_inserts(tablename,tables,written):
-	fields = tables[tablename]
-
-	if written.get(tablename)==1:
-		return
-
-	written[tablename] = 1
-
-	#
-	# dump prerequisites
-	#
-	for f in fields:
-		if f[4]:
-			print_creates_inserts(f[4][0],tables,written)
-
-	#
-	# dump table
-	#
-	if writeschema:
-		keyfields = filter(iskey,fields)
-
-		if len(keyfields)>1:
-			commalist = map(field_def,fields)
-			commalist.append('PRIMARY KEY (%s)'%','.join(map(lambda a:sqlfield(a[0]),keyfields)))
-		else:
-			commalist = map(field_def_pkey,fields)
-
-		sys.stdout.write('CREATE TABLE %s (\n\t%s\n);\n'%(sqlfield(tableprefix+tablename),',\n\t'.join(commalist)))
-
-		if commentsyntax==1:
-			for field in fields:
-				if field[5]<>None:
-					sys.stdout.write('COMMENT ON %s.%s IS %s;\n'%(sqlfield(tablename),sqlfield(field[1]),sqlstring(field[5])));
-
-	if writedata:
-		print_inserts(tablename,fields)
-
 	sys.stdout.write('\n');
 
-def print_drops(tablename,tables,dropped):
+def print_drops(tablename,prevtables,tables,dropped):
 	if dropped.get(tablename):
 		return
-
 	dropped[tablename] = 1
 
 	hasref = False
 	for tablename2,fields2 in tables.items():
 		for f2 in fields2:
 			if f2[4] and f2[4][0]==tablename:
-				print_drops(tablename2,tables,dropped)
+				print_drops(tablename2,prevtables,tables,dropped)
 
-	sys.stdout.write('DROP TABLE IF EXISTS %s;\n'%sqlfield(tableprefix+tablename))
+	if prevtables==None:
+		sys.stdout.write('DROP TABLE IF EXISTS %s;\n'%sqlfield(tableprefix+tablename))
+		sys.stdout.write('\n')
+
+def print_schema(tablename,prevtables,tables,written):
+	if written.get(tablename)==1:
+		return
+	written[tablename] = 1
+
+	#
+	# dump prerequisites
+	#
+	fields = tables[tablename]
+	for f in fields:
+		if f[4]:
+			print_schema(f[4][0],prevtables,tables,written)
+
+	#
+	# dump table
+	#
+	keyfields = filter(iskey,fields)
+
+	if prevtables==None or prevtables.get(tablename)==None:
+		if len(keyfields)>1:
+			commalist = map(field_def,fields)
+			commalist.append('PRIMARY KEY (%s)'%','.join(map(lambda a:sqlfield(a[0]),keyfields)))
+		else:
+			commalist = map(field_def_pkey,fields)
+
+		for f in fields:
+			if f[4]:
+				commalist.append('FOREIGN KEY (%s) REFERENCES %s(%s)'%(f[0],tableprefix+f[4][0],f[4][1]))
+
+		if mysql:
+			extra = ' ENGINE = InnoDB';
+
+		if commalist:
+			sys.stdout.write('CREATE TABLE %s (\n\t%s\n)%s;\n'%(sqlfield(tableprefix+tablename),',\n\t'.join(commalist),extra))
+			if commentsyntax==1:
+				for field in fields:
+					if field[5]<>None:
+						sys.stdout.write('COMMENT ON %s.%s IS %s;\n'%(sqlfield(tablename),sqlfield(field[1]),sqlstring(field[5])));
+			sys.stdout.write('\n');
+	else:
+		prevfields = prevtables[tablename]
+		extra = None
+		commalist = []
+		fa = None
+		for f in fields:
+			f2 = f[:4]+[None]+f[5:]
+			x = zip(prevfields,range(len(prevfields)))
+			pfs = filter(lambda pf: pf[0][0]==f[0], x)
+			if pfs:
+				if pfs[0][1]:
+					pfa = prevfields[pfs[0][1]-1][0]
+				else:
+					pfa = None
+				pf = pfs[0][0]
+				pf2 = pf[:4]+[None]+pf[5:]
+
+				if (f2,fa)<>(pf2,pfa):
+					s = 'MODIFY COLUMN '+field_def(f)
+					if fa==None:
+						s = s + ' FIRST'
+					else:
+						s = s + ' AFTER ' + sqlfield(fa)
+					commalist.append(s)
+
+				if pf[4]==None and f[4]<>None:
+					commalist.append('ADD FOREIGN KEY (%s) REFERENCES %s(%s)'%(f[0],tableprefix+f[4][0],f[4][1]))
+					if mysql:
+						extra = 'ENGINE = InnoDB'
+			else:
+				s = 'ADD COLUMN '+field_def(f)
+				if fa==None:
+					s = s + ' FIRST'
+				else:
+					s = s + ' AFTER ' + sqlfield(fa)
+				commalist.append(s)
+			fa = f[0]
+		for pf in prevfields:
+			fs = filter(lambda f: pf[0]==f[0], fields)
+			if not fs:
+				commalist.append('DROP COLUMN '+sqlfield(pf[0]))
+
+		if extra<>None:
+			commalist.append(extra)
+
+		if commalist:
+			sys.stdout.write('ALTER TABLE %s \n\t%s\n;\n'%(sqlfield(tableprefix+tablename),',\n\t'.join(commalist)))
+			sys.stdout.write('\n');
+
+def print_data(tablename,tables,written):
+	if written.get(tablename)==1:
+		return
+	written[tablename] = 1
+
+	#
+	# dump prerequisites
+	#
+	for f in tables[tablename]:
+		if f[4]:
+			print_data(f[4][0],tables,written)
+
+	#
+	# dump table
+	#
+	print_table(tablename,tables[tablename])
 
 if __name__ == '__main__':
-	written = {}
-	tables = readschema(sys.stdin)
+	if len(sys.argv)<2:
+		prevtables = None
+		tables = readschema(sys.stdin)
+
+	elif len(sys.argv)==2:
+		prevtables = None
+		f = open(sys.argv[1],'r')
+		tables = readschema(f)
+		f.close()
+
+	elif len(sys.argv)==3:
+		if sys.argv[1]=='-':
+			prevtables = readschema(sys.stdin)
+		else:
+			f = open(sys.argv[1],'r')
+			prevtables = readschema(f)
+			f.close()
+		f = open(sys.argv[2],'r')
+		tables = readschema(f)
+		f.close()
+
+	else:
+		sys.stderr.write('syntax: %s [[PREVSCHEMA|-] SCHEMA]\n'%sys.argv[0])
+		sys.exit(1)
 
 	if droptables:
 		dropped={}
 		for tablename in tables:
-			print_drops(tablename,tables,dropped)
-		sys.stdout.write('\n')
+			print_drops(tablename,prevtables,tables,dropped)
 
-	if writeschema or writedata:
+	if writeschema:
+		written = {}
 		for tablename,fields in tables.items():
-			print_creates_inserts(tablename,tables,written)
+			print_schema(tablename,prevtables,tables,written)
+
+	if writedata:
+		written = {}
+		for tablename,fields in tables.items():
+			print_data(tablename,tables,written)
